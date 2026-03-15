c9ac9eec4004fcebffb5e5186e9380e5ed672731
Bryan Montoya <55662285+bmontoyag@users.noreply.github.com>
2026-03-15 12:03:07 -0500

    fix postgres

--- backend/src/attention-workers/attention-workers.controller.ts
+++ backend/src/attention-workers/attention-workers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Patch, Param, Body } from '@nestjs/common';
+import { Controller, Patch, Param, Body, ParseIntPipe } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Controller('attention-workers')
@@ -6,9 +6,9 @@ export class AttentionWorkersController {
     constructor(private prisma: PrismaService) { }
 
     @Patch(':id')
-    async update(@Param('id') id: string, @Body() updateData: any) {
+    async update(@Param('id', ParseIntPipe) id: number, @Body() updateData: any) {
         return this.prisma.attentionWorker.update({
-            where: { id: +id },
+            where: { id },
             data: {
                 isPaid: updateData.isPaid,
                 paidAt: updateData.paidAt ? new Date(updateData.paidAt) : null,
